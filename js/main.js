f28fc5f06b3fb7bc603dd305b16a1676d64640ae
sanjay <sanjayprakashsivakasi@gmail.com>
2020-01-30 20:27:54 +0530

    Thu, Jan 30, 2020  8:27:54 PM

--- js/main.js
+++ js/main.js
@@ -3,30 +3,30 @@
  * Main js
  *
  * ------------------------------------------------------------------- 
- */ 
+ */
 
-(function($) {
+(function ($) {
 
 	"use strict";
 
 	/* --------------------------------------------------- */
 	/* Preloader
-	------------------------------------------------------ */ 
-   $(window).load(function() {
-      // will first fade out the loading animation 
-    	$("#loader").fadeOut("slow", function(){
+	------------------------------------------------------ */
+	$(window).load(function () {
+		// will first fade out the loading animation 
+		$("#loader").fadeOut("slow", function () {
 
-        // will fade out the whole DIV that covers the website.
-        $("#preloader").delay(300).fadeOut("slow");
+			// will fade out the whole DIV that covers the website.
+			$("#preloader").delay(300).fadeOut("slow");
 
-      }); 
-  	})
+		});
+	})
 
 
-  	/*---------------------------------------------------- */
+	/*---------------------------------------------------- */
 	/* FitVids
-	------------------------------------------------------ */ 
-  	$(".fluid-video-wrapper").fitVids();
+	------------------------------------------------------ */
+	$(".fluid-video-wrapper").fitVids();
 
 
 	/* --------------------------------------------------- */
@@ -36,11 +36,11 @@
 		transition: 'fade',
 		transitionDuration: 2500,
 		delay: 5000,
-    	slides: [
-       	{ src: "images/slides/03.jpg" },
-        	{ src: "images/slides/02.jpg" },
-        	{ src: "images/slides/01.jpg" }
-    	]
+		slides: [
+			{ src: "images/slides/03.jpg" },
+			{ src: "images/slides/02.jpg" },
+			{ src: "images/slides/01.jpg" }
+		]
 	});
 
 
@@ -48,58 +48,58 @@
 	/*  Particle JS
 	------------------------------------------------------ */
 	$('.home-particles').particleground({
-	   dotColor: '#fff',
-	   lineColor: '#555555',
-	   particleRadius: 6,
-	   curveLines: true,
-	   density: 10000,
-	   proximity: 110
-	}); 
+		dotColor: '#ffffff',
+		lineColor: '#555555',
+		particleRadius: 10,
+		curveLines: true,
+		density: 10000,
+		proximity: 110
+	});
 
 
 	/*-----------------------------------------------------*/
 	/* tabs
-  	-------------------------------------------------------*/	
+  	-------------------------------------------------------*/
 	$(".tab-content").hide();
 	$(".tab-content").first().show();
 
 	$("ul.tabs li").click(function () {
-	   $("ul.tabs li").removeClass("active");
-	   $(this).addClass("active");
-	   $(".tab-content").hide();
-	   var activeTab = $(this).attr("data-id");
-	   $("#" + activeTab).fadeIn(700);
+		$("ul.tabs li").removeClass("active");
+		$(this).addClass("active");
+		$(".tab-content").hide();
+		var activeTab = $(this).attr("data-id");
+		$("#" + activeTab).fadeIn(700);
 	});
 
 
 	/*----------------------------------------------------*/
-  	/* Smooth Scrolling
-  	------------------------------------------------------*/
-  	$('.smoothscroll').on('click', function (e) {
-	 	
-	 	e.preventDefault();
+	/* Smooth Scrolling
+	------------------------------------------------------*/
+	$('.smoothscroll').on('click', function (e) {
 
-   	var target = this.hash,
-    	$target = $(target);
+		e.preventDefault();
 
-    	$('html, body').stop().animate({
-       	'scrollTop': $target.offset().top
-      }, 800, 'swing', function () {
-      	window.location.hash = target;
-      });
+		var target = this.hash,
+			$target = $(target);
 
-  	});
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top
+		}, 800, 'swing', function () {
+			window.location.hash = target;
+		});
+
+	});
 
 
-  	/* --------------------------------------------------- */
+	/* --------------------------------------------------- */
 	/*  Placeholder Plugin Settings
 	------------------------------------------------------ */
-	$('input, textarea, select').placeholder()  
+	$('input, textarea, select').placeholder()
 
 
-  	/*---------------------------------------------------- */
-   /* ajaxchimp
-	------------------------------------------------------ */
+	/*---------------------------------------------------- */
+	/* ajaxchimp
+	 ------------------------------------------------------ */
 
 	// Example MailChimp url: http://xxx.xxx.list-manage.com/subscribe/post?u=xxx&id=xxx
 	var mailChimpURL = 'http://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e65110b38d'
@@ -107,7 +107,7 @@
 	$('#mc-form').ajaxChimp({
 
 		language: 'es',
-	   url: mailChimpURL
+		url: mailChimpURL
 
 	});
 
@@ -123,13 +123,13 @@
 	//  5: 'This email address looks fake or invalid. Please enter a real email address'
 
 	$.ajaxChimp.translations.es = {
-	  'submit': 'Submitting...',
-	  0: '<i class="fa fa-check"></i> We have sent you a confirmation email',
-	  1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
-	  2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-	  3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-	  4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-	  5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
+		'submit': 'Submitting...',
+		0: '<i class="fa fa-check"></i> We have sent you a confirmation email',
+		1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
+		2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+		3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+		4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+		5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
 	}
 
 
@@ -141,47 +141,47 @@
 	$('#contactForm').validate({
 
 		/* submit via ajax */
-		submitHandler: function(form) {
+		submitHandler: function (form) {
 
 			var sLoader = $('#submit-loader');
 
-			$.ajax({      	
+			$.ajax({
 
-		      type: "POST",
-		      url: "inc/sendEmail.php",
-		      data: $(form).serialize(),
-		      beforeSend: function() { 
+				type: "POST",
+				url: "inc/sendEmail.php",
+				data: $(form).serialize(),
+				beforeSend: function () {
 
-		      	sLoader.fadeIn(); 
+					sLoader.fadeIn();
 
-		      },
-		      success: function(msg) {
+				},
+				success: function (msg) {
 
-	            // Message was sent
-	            if (msg == 'OK') {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').hide();
-	               $('#contactForm').fadeOut();
-	               $('#message-success').fadeIn();   
-	            }
-	            // There was an error
-	            else {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').html(msg);
-		            $('#message-warning').fadeIn();
-	            }
+					// Message was sent
+					if (msg == 'OK') {
+						sLoader.fadeOut();
+						$('#message-warning').hide();
+						$('#contactForm').fadeOut();
+						$('#message-success').fadeIn();
+					}
+					// There was an error
+					else {
+						sLoader.fadeOut();
+						$('#message-warning').html(msg);
+						$('#message-warning').fadeIn();
+					}
 
-		      },
-		      error: function() {
+				},
+				error: function () {
 
-		      	sLoader.fadeOut(); 
-		      	$('#message-warning').html("Something went wrong. Please try again.");
-		         $('#message-warning').fadeIn();
+					sLoader.fadeOut();
+					$('#message-warning').html("Something went wrong. Please try again.");
+					$('#message-warning').fadeIn();
 
-		      }
+				}
 
-	      });     		
-  		}
+			});
+		}
 
 	});
 
@@ -192,18 +192,18 @@
 	var finalDate = '2018/01/01';
 
 	$('div#counter').countdown(finalDate)
-   	.on('update.countdown', function(event) {
+		.on('update.countdown', function (event) {
 
-   		$(this).html(event.strftime('<div class=\"half\">' +
-   											 '<span>%D <sup>days</sup></span>' + 
-   										 	 '<span>%H <sup>hours</sup></span>' + 
-   										 	 '</div>' +
-   										 	 '<div class=\"half\">' +
-   										 	 '<span>%M <sup>mins</sup></span>' +
-   										 	 '<span>%S <sup>secs</sup></span>' +
-   										 	 '</div>'));
+			$(this).html(event.strftime('<div class=\"half\">' +
+				'<span>%D <sup>days</sup></span>' +
+				'<span>%H <sup>hours</sup></span>' +
+				'</div>' +
+				'<div class=\"half\">' +
+				'<span>%M <sup>mins</sup></span>' +
+				'<span>%S <sup>secs</sup></span>' +
+				'</div>'));
 
-   });     
- 
+		});
+
 
 })(jQuery);
